9a655529c05694bee6c470955812428dcd28fbd9
Vadym-Lopatka <vadim.lopatka@gmail.com>
2020-05-09 18:57:03 +0300

    init

---
+++ src/main/kotlin/com/ihill/app/IntellectHillApplication.kt
@@ -0,0 +1,11 @@
+package com.ihill.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IntellectHillApplication
+
+fun main(args: Array<String>) {
+	runApplication<IntellectHillApplication>(*args)
+}
---
+++ src/test/kotlin/com/ihill/app/IntellectHillApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ihill.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IntellectHillApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
