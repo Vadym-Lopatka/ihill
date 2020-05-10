08f0472a65874dc86aa109ae225a1dfbc7008553
vlopatka <vlopatka@overonix.com>
2020-05-10 13:29:18 +0300

    [GM-1] - added Logger

--- src/main/kotlin/com/ihill/app/gameRequest/web/GameRequestResource.kt
+++ src/main/kotlin/com/ihill/app/gameRequest/web/GameRequestResource.kt
@@ -1,5 +1,6 @@
 package com.ihill.app.gameRequest.web
 
+import com.ihill.app.config.getLogger
 import com.ihill.app.gameRequest.service.GameRequestService
 import com.ihill.app.gameRequest.domain.GameRequest
 import com.ihill.app.gameRequest.web.GameRequestResource.Companion.GAME_REQUEST_URL
@@ -12,9 +13,11 @@ import javax.validation.Valid
 @RestController
 @RequestMapping(GAME_REQUEST_URL)
 class GameRequestResource (val service: GameRequestService) {
+    private val log = getLogger(javaClass)
 
     @PostMapping
     fun openGameRequest(@RequestBody @Valid request: OpenGameRequest): GameRequest {
+        log.debug("[API] request to open GameRequest by request: $request")
         return service.openGameRequest(request.initiatorUuid)
     }
 
